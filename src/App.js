3b8217a47c45479ac340416114a1411e8fae9652
Michael Rodgers <mail@mjrodge.me>
2018-12-13 12:18:59 -0500

    Added Firebase Config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 
 import { Header } from './components/common';
+import LoginForm from './components/loginForm';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAOkOzTbhajSY7btbxohSG70FfgIb60I_w',
+  authDomain: 'react-native-auth-test-a1d74.firebaseapp.com',
+  databaseURL: 'https://react-native-auth-test-a1d74.firebaseio.com',
+  projectId: 'react-native-auth-test-a1d74',
+  storageBucket: 'react-native-auth-test-a1d74.appspot.com',
+  messagingSenderId: '962256974612',
+};
 
 class App extends Component {
   componentWillMount() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyAOkOzTbhajSY7btbxohSG70FfgIb60I_w',
-      authDomain: 'react-native-auth-test-a1d74.firebaseapp.com',
-      databaseURL: 'https://react-native-auth-test-a1d74.firebaseio.com',
-      projectId: 'react-native-auth-test-a1d74',
-      storageBucket: 'react-native-auth-test-a1d74.appspot.com',
-      messagingSenderId: '962256974612',
-    });
+    firebase.initializeApp(firebaseConfig);
   }
   render() {
     return (
       <View>
         <Header headerText="FirebaseAuth" />
-        <Text>Apppppp</Text>
+        <LoginForm />
       </View>
     );
   }
