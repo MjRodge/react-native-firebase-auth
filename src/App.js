4038ef35797cc0dffd7072ed36e12b54b477a231
Michael Rodgers <mail@mjrodge.me>
2018-12-17 12:53:29 -0500

    Conditionally Rendering
    Will check if user is logged in, if so will direct to the logout component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import {
+  Header,
+  Button,
+  Spinner,
+  Card,
+  CardSection,
+} from './components/common';
 import LoginForm from './components/loginForm';
 
 const firebaseConfig = {
@@ -15,14 +21,50 @@ const firebaseConfig = {
 };
 
 class App extends Component {
+  state = {
+    loggedIn: null,
+  };
+
   componentWillMount() {
     firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent = () => {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button>Log Out</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <Card>
+            <CardSection>
+              <Spinner size="large" />
+            </CardSection>
+          </Card>
+        );
+    }
+  };
+
   render() {
     return (
       <View>
         <Header headerText="FirebaseAuth" />
-        <LoginForm />
+        <View>{this.renderContent()}</View>
       </View>
     );
   }
